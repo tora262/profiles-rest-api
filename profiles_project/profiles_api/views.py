ff20c07ad7badb2aa4ad7aa1ed1d07067baaba43
tora262 <tth.hust@gmail.com>
2021-01-08 21:23:18 +0700

    Add viewset to profiles api

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -63,3 +63,54 @@ class HelloApiView(APIView):
         """
         return Response({"method":"DELETE"})
 
+class HelloViewSet(ViewSet):
+    """
+    Test API ViewSet
+    """
+    serializer_class = serializers.HelloSerializer
+    def list(self, request):
+        """
+        Return a list from db
+        """
+        a_viewset = [
+            'Uses actions (list, create, retrive, update, partial_update, delete)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+        ]   
+
+        return Response({'message' : 'Hello', 'a_viewset' : a_viewset})
+
+    def create(self, request):
+        """
+        Create a new hello message
+        """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.status.HTTP_400_BAD_REQUEST)
+
+    
+    def retrieve(self, request, pk=None):
+        """
+        Handle getting an object by its ID
+        """
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """
+        Handle updating an object by its ID
+        """
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """
+        Handle getting an object by its ID
+        """
+
+        return Response({'http_method': 'PATCH'})
